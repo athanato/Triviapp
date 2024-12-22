83e73b03ca9a59a457fcb691aa15e56cb8238b39
VasoUnipi <vaso_isouki@yahoo.com>
2024-12-22 23:42:18 +0200

    REST Web Service corrections in Test

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -1,9 +1,5 @@
-package org.example;
-
 import org.example.DemoGetRESTAPI;
 
-public class Test {
     public static void main(String[] args) {
         DemoGetRESTAPI.demoGetRESTAPI();  // Καλείς τη μέθοδο demoGetRESTAPI από την κλάση DemoGetRESTAPI
     }
-}
