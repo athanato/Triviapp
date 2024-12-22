9737bd9476fa524a0dd22c683975737268b5e12f
VasoUnipi <vaso_isouki@yahoo.com>
2024-12-22 23:37:51 +0200

    REST Web Service

---
+++ src/main/java/org/example/Test.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.example.DemoGetRESTAPI;
+
+public class Test {
+    public static void main(String[] args) {
+        DemoGetRESTAPI.demoGetRESTAPI();  // Καλείς τη μέθοδο demoGetRESTAPI από την κλάση DemoGetRESTAPI
+    }
+}
