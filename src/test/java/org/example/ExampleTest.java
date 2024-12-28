2df5c4992e59d0737bb164df5e71638445ed5533
VasoUnipi <vaso_isouki@yahoo.com>
2024-12-28 23:03:49 +0200

    Created JUnit tests for Example.java and Result.Java classes

---
+++ src/test/java/org/example/ExampleTest.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExampleTest {
+
+    @Test
+    void getResponseCode() {
+    }
+
+    @Test
+    void setResponseCode() {
+    }
+
+    @Test
+    void getResults() {
+    }
+
+    @Test
+    void setResults() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
---
+++ src/test/java/org/example/ResultTest.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ResultTest {
+
+    @Test
+    void getType() {
+    }
+
+    @Test
+    void setType() {
+    }
+
+    @Test
+    void getDifficulty() {
+    }
+
+    @Test
+    void setDifficulty() {
+    }
+
+    @Test
+    void getCategory() {
+    }
+
+    @Test
+    void setCategory() {
+    }
+
+    @Test
+    void getQuestion() {
+    }
+
+    @Test
+    void setQuestion() {
+    }
+
+    @Test
+    void getCorrect_answer() {
+    }
+
+    @Test
+    void setCorrect_answer() {
+    }
+
+    @Test
+    void getIncorrect_answers() {
+    }
+
+    @Test
+    void setIncorrect_answers() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
